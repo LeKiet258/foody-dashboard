23cb92b74f4c22fc052a11d4ca05b1100a17d726
unknown <nguyentuongkhaitqd@gmail.com>
2022-11-18 12:02:17 +0700

    Update html searchPage and POST method

--- search/views.py
+++ search/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -6,9 +6,19 @@ from django.http import HttpResponse
 
 def search(request):
     context = {}
+    if request.method == 'POST':
+        search = request.POST.get('search')
+        print(search)
+        return redirect('result')
+
     return render(request, 'search/searchPage.html', context)
 
 
 def result(request):
+    stores = []
     context = {}
+
+    # if len(stores) == 0:
+    #     return render(request, 'search/emptyPage.html', context)
+
     return render(request, 'search/searchResult.html', context)
