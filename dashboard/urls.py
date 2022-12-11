c43050ddc1564d1886e935dd23e1f2ed2c827509
Khanh Dinh <dinhnguyenkhanh210401@gmail.com>
2022-12-11 12:28:20 +0700

    fix ui

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"), 
     path('/<str:page>/', views.home), 
-    path('dashboard/<int:res_id>', views.dashboard, name='dashboard'),
+    path('dashboard/<int:res_id>/', views.dashboard, name='dashboard'),
     path('compare/', views.compare_2_vendors, name='compare')
 ]
